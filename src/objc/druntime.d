837f77dad50d325631373e316e3afebef438c10d
Michel Fortin <michel.fortin@michelf.com>
2011-03-01 14:21:55 -0500

    Added runtime function for alloc/allocWithZone and Objective-C dynamic casts.

--- src/objc/druntime.d
+++ src/objc/druntime.d
@@ -18,3 +18,45 @@ module objc.druntime;
 version (D_ObjC) {}
 else static assert(0, "Compiler does not support the Objective-C object model");
 
+import objc.runtime;
+
+// Replacement allocators for D/Objective-C objects to allow static initializers to work
+
+extern (C):
+
+id _dobjc_alloc(Class cls, SEL _cmd)
+{
+    id object = class_createInstance(cls, 0);
+    id __selector() _dobjc_preinit = cast(id __selector())"_dobjc_preinit";
+    return _dobjc_preinit(object);
+}
+
+id _dobjc_allocWithZone(Class cls, SEL _cmd, void* zone)
+{
+    static if (__traits(compiles, class_createInstanceFromZone(cls, 0, zone)))
+    {   
+        id object = class_createInstanceFromZone(cls, 0, zone);
+        id __selector() _dobjc_preinit = cast(id __selector())"_dobjc_preinit";
+        return _dobjc_preinit(object);
+    }
+    else
+        return _dobjc_alloc(cls, _cmd);
+}
+
+// Dynamic casts stubs
+
+id _dobjc_dynamic_cast(id obj, Class cls)
+{
+    id __selector(Class) isKindOfClass = cast(id __selector(Class))"isKindOfClass:";
+    if (isKindOfClass(obj, cls))
+        return obj;
+    return null;
+}
+
+id _dobjc_interface_cast(id obj, Protocol p)
+{
+    id __selector(Protocol) conformsToProtocol = cast(id __selector(Protocol))"conformsToProtocol:";
+    if (conformsToProtocol(obj, p))
+        return obj;
+    return null;
+}
