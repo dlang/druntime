b2ee9ae93d475339dfc510d9dabdc3721972c122
k-hara <k.hara.pg@gmail.com>
2012-07-17 11:51:17 +0900

    Revert "Merge pull request #273 from 9rnsr/strict_const"
    This reverts commit 90cccc882e9a5e970e58bf0d643933aea72207f2, reversing
    changes made to de8d474d6c506d27e6dfb133fa062d2f1179d970.

--- src/core/runtime.d
+++ src/core/runtime.d
@@ -462,7 +462,7 @@ Throwable.TraceInfo defaultTraceHandler( void* ptr = null )
                 return ret;
             }
 
-            override string toString() const
+            override string toString()
             {
                 string buf;
                 foreach( i, line; this )
