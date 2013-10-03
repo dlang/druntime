8ebdfe349c7d1817b2b2c31c9741d00c49837b7c
Martin Nowak <code@dawg.eu>
2013-10-03 20:38:26 +0200

    kill console

--- src/core/runtime.d
+++ src/core/runtime.d
@@ -382,28 +382,13 @@ extern (C) bool runModuleUnitTests()
         }
     }
 
-    static struct Console
-    {
-        Console opCall( in char[] val )
-        {
-            version( Windows )
-            {
-                DWORD count = void;
-                assert(val.length <= uint.max, "val must be less than or equal to uint.max");
-                WriteFile( GetStdHandle( 0xfffffff5 ), val.ptr, cast(uint)val.length, &count, null );
-            }
-            else version( Posix )
-            {
-                write( 2, val.ptr, val.length );
-            }
-            return this;
-        }
-    }
-
-    static __gshared Console console;
-
     if( Runtime.sm_moduleUnitTester is null )
     {
+        void printErr(const(char)[] buf)
+        {
+            .fprintf(.stderr, "%.s", cast(int)buf.length, buf.ptr);
+        }
+
         size_t failed = 0;
         foreach( m; ModuleInfo )
         {
@@ -419,7 +404,7 @@ extern (C) bool runModuleUnitTests()
                     }
                     catch( Throwable e )
                     {
-                        console( e.toString() )( "\n" );
+                        e.toString(&printErr); printErr("\n");
                         failed++;
                     }
                 }
