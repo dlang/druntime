13c1f5e4f5fe5f5c19838a437d42e1a596353aa1
Martin Nowak <code@dawg.eu>
2016-12-30 16:14:08 +0100

    add comment why this was written in such a way

--- src/object.d
+++ src/object.d
@@ -3168,6 +3168,8 @@ bool _ArrayEq(T1, T2)(T1[] a1, T2[] a2)
     if (a1.length != a2.length)
         return false;
 
+    // This is function is used as a compiler intrinsic and explicitly written
+    // in a lowered flavor to use as few CTFE instructions as possible.
     auto idx = 0;
     auto length = a1.length;
 
