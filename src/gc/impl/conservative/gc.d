d5088da1f0c1517b50954f5527f98c23f88c1644
Nemanja Boric <4burgos@gmail.com>
2017-07-13 17:11:04 +0000

    Rename ConservativeGC._in_Finalizer to _is_Locked
    Since this field was used to prevent recursive calls to GC
    to itself while is locked, not just when running inside finalizer,
    this field is renamed to better describe its purpose.

--- src/gc/impl/conservative/gc.d
+++ src/gc/impl/conservative/gc.d
@@ -259,12 +259,12 @@ class ConservativeGC : GC
 
     import core.internal.spinlock;
     static gcLock = shared(AlignedSpinLock)(SpinLock.Contention.lengthy);
-    static bool _inFinalizer;
+    static bool _isLocked;
 
     // lock GC, throw InvalidMemoryOperationError on recursive locking during finalization
     static void lockNR() @nogc nothrow
     {
-        if (_inFinalizer)
+        if (_isLocked)
             onInvalidMemoryOperationError();
         gcLock.lock();
     }
@@ -817,7 +817,7 @@ class ConservativeGC : GC
 
     void free(void *p) nothrow
     {
-        if (!p || _inFinalizer)
+        if (!p || _isLocked)
         {
             return;
         }
@@ -1120,7 +1120,7 @@ class ConservativeGC : GC
 
     bool inFinalizer() nothrow
     {
-        return _inFinalizer;
+        return _isLocked;
     }
 
 
@@ -1500,8 +1500,8 @@ struct Gcx
      */
     void runFinalizers(in void[] segment) nothrow
     {
-        ConservativeGC._inFinalizer = true;
-        scope (failure) ConservativeGC._inFinalizer = false;
+        ConservativeGC._isLocked = true;
+        scope (failure) ConservativeGC._isLocked = false;
 
         foreach (pool; pooltable[0 .. npools])
         {
@@ -1518,7 +1518,7 @@ struct Gcx
                 spool.runFinalizers(segment);
             }
         }
-        ConservativeGC._inFinalizer = false;
+        ConservativeGC._isLocked = false;
     }
 
     Pool* findPool(void* p) pure nothrow
@@ -2408,12 +2408,12 @@ struct Gcx
             start = stop;
         }
 
-        ConservativeGC._inFinalizer = true;
+        ConservativeGC._isLocked = true;
         size_t freedLargePages=void;
         {
-            scope (failure) ConservativeGC._inFinalizer = false;
+            scope (failure) ConservativeGC._isLocked = false;
             freedLargePages = sweep();
-            ConservativeGC._inFinalizer = false;
+            ConservativeGC._isLocked = false;
         }
 
         if (config.profile)
