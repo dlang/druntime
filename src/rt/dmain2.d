fb7c309829add21fa7e7e84b033b6a7e8dd8a0ce
Sebastian Wilzbach <seb@wilzba.ch>
2018-07-11 06:04:53 +0200

    Handle page protection errors by default as D errors on Linux

--- src/rt/dmain2.d
+++ src/rt/dmain2.d
@@ -441,6 +441,14 @@ private extern (C) int _d_run_main2(char[][] args, size_t totalArgsLength, MainF
 
     auto useExceptionTrap = parseExceptionOptions();
 
+    // Handle page protection errors using D errors (exceptions) when supported
+    version (linux)
+    {
+        import etc.linux.memoryerror : registerMemoryErrorHandler;
+        if (parseMemoryErrorOptions())
+            registerMemoryErrorHandler();
+    }
+
     version (Windows)
     {
         if (IsDebuggerPresent())
@@ -552,6 +560,18 @@ private auto parseExceptionOptions()
     const optName = "trapExceptions";
     auto option = rt_configOption(optName);
     auto trap = rt_trapExceptions;
+    if (option.length)
+        rt_parseOption(optName, option, trap, "");
+    return trap;
+}
+
+private auto parseMemoryErrorOptions()
+{
+    import rt.config : rt_configOption;
+    import core.internal.parseoptions : rt_parseOption;
+    enum optName = "memoryError";
+    auto option = rt_configOption(optName);
+    bool trap;
     if (option.length)
         rt_parseOption(optName, option, trap, "");
     return trap;
