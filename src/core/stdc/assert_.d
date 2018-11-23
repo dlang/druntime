4f8b638f9d18c4c980c976d3ad1508dd2d07b394
Nathan Sashihara <21227491+n8sh@users.noreply.github.com>
2018-11-23 07:46:56 -0500

    Fix Issue 19425 - Add core.stdc.assert_ support for Haiku, NetBSD, OpenBSD

--- src/core/stdc/assert_.d
+++ src/core/stdc/assert_.d
@@ -46,6 +46,20 @@ else version (OSX)
      */
     void __assert_rtn(const(char)* func, const(char)* file, uint line, const(char)* exp);
 }
+else version (OpenBSD)
+{
+    // https://github.com/openbsd/src/blob/master/include/assert.h
+    void __assert(const(char)*, int, const(char)*);
+    void __assert2(const(char)*, int, const(char)*, const(char)*);
+}
+else version (NetBSD)
+{
+    // https://github.com/NetBSD/src/blob/trunk/include/assert.h
+    void __assert(const(char)*, int, const(char)*);
+    void __assert13(const(char)*, int, const(char)*, const(char)*);
+    void __diagassert(const(char)*, int, const(char)*);
+    void __diagassert13(const(char)*, int, const(char)*, const(char)*);
+}
 else version (FreeBSD)
 {
     /***
@@ -89,6 +103,12 @@ else version (CRuntime_UClibc)
 else version (Solaris)
 {
     void __assert_c99(const(char)* exp, const(char)* file, uint line, const(char)* func);
+}
+else version (Haiku)
+{
+    // https://github.com/haiku/haiku/blob/master/headers/posix/assert.h
+    void __assert_fail(const(char)* exp, const(char)* file, uint line, const(char)* func);
+    void __assert_perror_fail(int errnum, const(char)* file, uint line, const(char)* func);
 }
 else
 {
