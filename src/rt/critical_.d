c592d264987273fe407e580f1330786121a0ae8e
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2019-07-06 17:12:09 +0800

    Fix some usages of

--- src/rt/critical_.d
+++ src/rt/critical_.d
@@ -25,7 +25,8 @@ extern (C) void _d_critical_init()
 
 extern (C) void _d_critical_term()
 {
-    for (auto p = head; p; p = p.next)
+    auto h = cast()head;
+    for (auto p = h; p; p = p.next)
         destroyMutex(cast(Mutex*)&p.mtx);
 }
 
