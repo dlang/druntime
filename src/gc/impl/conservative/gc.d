6c59364a934dcc2f321dde89eba487dd88c367a2
Igor Khasilev <igor.khasilev@gmail.com>
2019-10-02 10:53:37 +0300

    fix issue 20256

--- src/gc/impl/conservative/gc.d
+++ src/gc/impl/conservative/gc.d
@@ -2874,15 +2874,16 @@ struct Gcx
 
     void scanBackground() nothrow
     {
-        version(linux) {
+        version(linux)
+        {
             import core.sys.posix.signal;
 
             // block all signals, so that scanThread will not receive a signal not intended for him.
             // see https://issues.dlang.org/show_bug.cgi?id=20256
             sigset_t new_mask;
             sigfillset(&new_mask);
-			int sigmask_rc = pthread_sigmask(SIG_BLOCK, &new_mask, null);
-			assert(sigmask_rc == 0, "failed to set up GC scan thread sigmask");
+            int sigmask_rc = pthread_sigmask(SIG_BLOCK, &new_mask, null);
+            assert(sigmask_rc == 0, "failed to set up GC scan thread sigmask");
 		}
         while (!stopGC)
         {
