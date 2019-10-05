302137cfeb4e922524592ce9caf7d4fb45b2ee23
سليمان السهمي  (Suleyman Sahmi) <sahmi.soulaimane@gmail.com>
2019-10-05 18:14:45 +0000

    Type info for rvalue type modifier

--- src/object.d
+++ src/object.d
@@ -1497,6 +1497,14 @@ class TypeInfo_Inout : TypeInfo_Const
     }
 }
 
+class TypeInfo_Rvalue : TypeInfo_Const
+{
+    override string toString() const
+    {
+        return cast(string) ("@rvalue(" ~ base.toString() ~ ")");
+    }
+}
+
 // Contents of Moduleinfo._flags
 enum
 {
