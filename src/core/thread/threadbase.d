0ee872f8a0a30cb657b3f0901195b22c069cfe2f
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-06-10 21:23:10 +0700

    unused windows imports removed

--- src/core/thread/threadbase.d
+++ src/core/thread/threadbase.d
@@ -769,23 +769,6 @@ package(core.thread):
     }
 }
 
-//FIXME: remove all these imports:
-version (Windows)
-{
-    import core.sys.windows.basetsd;
-    import core.stdc.stdint : uintptr_t; // for _beginthreadex decl below
-    import core.stdc.stdlib;             // for malloc, atexit
-    import core.sys.windows.basetsd /+: HANDLE+/;
-    import core.sys.windows.threadaux /+: getThreadStackBottom, impersonate_thread, OpenThreadHandle+/;
-    import core.sys.windows.winbase /+: CloseHandle, CREATE_SUSPENDED, DuplicateHandle, GetCurrentThread,
-        GetCurrentThreadId, GetCurrentProcess, GetExitCodeThread, GetSystemInfo, GetThreadContext,
-        GetThreadPriority, INFINITE, ResumeThread, SetThreadPriority, Sleep,  STILL_ACTIVE,
-        SuspendThread, SwitchToThread, SYSTEM_INFO, THREAD_PRIORITY_IDLE, THREAD_PRIORITY_NORMAL,
-        THREAD_PRIORITY_TIME_CRITICAL, WAIT_OBJECT_0, WaitForSingleObject+/;
-    import core.sys.windows.windef /+: TRUE+/;
-    import core.sys.windows.winnt /+: CONTEXT, CONTEXT_CONTROL, CONTEXT_INTEGER+/;
-}
-
 ///
 unittest
 {
