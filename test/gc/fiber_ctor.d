1132f98a784e13db94caaeeccb47c651c3b05b09
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-10-25 23:35:41 +0700

    fiber test: mem sizze increased

--- test/gc/fiber_ctor.d
+++ test/gc/fiber_ctor.d
@@ -1,12 +1,12 @@
 // Check avoid of destruction of fiber if GC collection will be invoked during Fiber construction
 
-extern(C) __gshared string[] rt_options = [ "gcopt=minPoolSize:4K  maxPoolSize:1M incPoolSize:4K" ];
+extern(C) __gshared string[] rt_options = [ "gcopt=minPoolSize:1M incPoolSize:4K" ];
 
 import core.memory : GC;
 
 void main()
 {
-    char[] buf = new char[1024 * 16];
+    char[] buf = new char[1024 * 1024 * 16];
 
     auto f = new Fiber( (){} );
     f.call();
