72c8d2ef8cba5cf0696c166246f7c292bf1e50a2
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-10-25 23:18:57 +0700

    Fiber construction: GC test added

---
+++ test/gc/fiber_ctor.d
@@ -0,0 +1,13 @@
+// Check avoid of destruction of fiber if GC collection will be invoked during Fiber construction
+
+extern(C) __gshared string[] rt_options = [ "gcopt=minPoolSize:4K  maxPoolSize:1M incPoolSize:4K" ];
+
+import core.memory : GC;
+
+void main()
+{
+    char[] buf = new char[1024 * 16];
+
+    auto f = new Fiber( (){} );
+    f.call();
+}
