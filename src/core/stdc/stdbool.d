b08101c8420f6c4203191d52ce1928fde395a222
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-06-03 04:32:56 -0500

    add stdbool.d header inside core.stdc

---
+++ src/core/stdc/stdbool.d
@@ -0,0 +1,20 @@
+/**
+ * D header file for C99.
+ *
+ * Source:    $(DRUNTIMESRC core/stdc/stdbool.d)
+ * Standards: ISO/IEC 9899:1999 (E)
+ */
+module core.stdc.stdbool;
+
+extern (C):
+@trusted: // Types and constants only.
+nothrow:
+@nogc:
+
+alias _Bool = bool;
+
+// bool is already defined in D
+// true is already defined in D
+// false is already defined in D
+
+enum __bool_true_false_are_defined = 1;
